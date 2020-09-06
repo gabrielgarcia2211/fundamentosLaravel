b92cbf6783b744ebdbfc685bf5010521060ce6a5
gabriel <gabrielarturogq@ufps.edu.co>
2020-09-05 19:33:51 -0500

    primer commit

---
+++ app/Task.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $table = 'tasks';
+
+    protected $fillable = [
+        'title'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+
+    public function user(){
+        return $this->belongsTo(User::class)
+    }
+}
